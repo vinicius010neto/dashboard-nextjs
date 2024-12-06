80a3123ebb572d043487bb1c66b5b1fe1f72fc1b
vinicius010neto <74117559+vinicius010neto@users.noreply.github.com>
2024-12-06 11:56:28 -0300

    .

--- pages/landing.js
+++ pages/landing.js
@@ -15,7 +15,7 @@ export default function Landing() {
           className="absolute top-0 w-full h-full bg-center bg-cover"
           style={{
             backgroundImage:
-              "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')",
+              "url('https://images6.alphacoders.com/135/1351738.jpeg')",
           }}
         >
           <span
@@ -129,9 +129,9 @@ export default function Landing() {
                 JavaScript.
               </p>
               <Link href="/" className="font-bold text-blueGray-700 mt-8">
-                
-                  Check Notus NextJS!
-                
+
+                Check Notus NextJS!
+
               </Link>
             </div>
 
